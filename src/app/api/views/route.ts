f6a63f6518a8650d77b85287c3d0aae02b0163d5
sam-ayo <samuelayomideadeoye@gmail.com>
2025-01-27 13:14:02 -0330

    comment out view endpoint

--- src/app/api/views/route.ts
+++ src/app/api/views/route.ts
@@ -2,12 +2,17 @@ import { NextRequest, NextResponse } from "next/server";
 import { cacheClient } from "../../../../redis";
 
 export async function POST(request: NextRequest) {
-    const {postId} = await request.json();
-    const noOfViews = await cacheClient.get(postId)
-    if(!noOfViews) {
-        await cacheClient.set(postId, 1);
-        return NextResponse.json({message: 'First view 🚀'})
-    }
-    await cacheClient.set(postId, parseInt(noOfViews, 10)+1)
-    return NextResponse.json({message: "+1 view 🔥"})
+    // const {postId} = await request.json();
+    // try {
+    // const noOfViews = await cacheClient.get(postId)
+    // if(!noOfViews) {
+    //     await cacheClient.set(postId, 1);
+    //     return NextResponse.json({message: 'First view 🚀'})
+    // }
+    // await cacheClient.set(postId, parseInt(noOfViews, 10)+1)
+    // return NextResponse.json({message: "+1 view 🔥"})
+    // } catch (error) {
+    //     console.log(error)
+    //     return NextResponse.json({message: error})
+    // }
 }
