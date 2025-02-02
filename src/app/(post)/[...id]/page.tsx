b813580fd67285fc7c8b8a6052f7cbde93ac0da3
sam-ayo <samuelayomideadeoye@gmail.com>
2025-02-01 23:40:30 -0330

    move post retrieval logic to api

--- src/app/(post)/[...id]/page.tsx
+++ src/app/(post)/[...id]/page.tsx
@@ -3,7 +3,6 @@ import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import { tags } from './html-tag-styles';
 import { Header, PostContent } from './header';
-import { getPost } from '@/app/get-posts';
 
 export default async function Page({
   params,
@@ -11,7 +10,16 @@ export default async function Page({
   params: Promise<{ id: string[] }>;
 }) {
   const postId = (await params).id;
-  const post = await getPost({ postId: postId.join('/') });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts?postId=${postId.join('/')}`,
+  );
+
+  if (!res.ok) {
+    return <div>Could not get post</div>;
+  }
+  const post = (await res.json()) as PostContent;
+  post.date = new Date(post.date);
+
   if (!post) {
     return <div className="text-center py-8">Post not found</div>;
   }
--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,7 +1,13 @@
-import { getPostPreview } from '@/app/get-posts';
-import { NextResponse } from 'next/server';
+import { getPost, getPostPreview } from '@/app/get-posts';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
-  const postPreviews = await getPostPreview();
-  return NextResponse.json(postPreviews);
+export async function GET(request: NextRequest) {
+  const url = new URL(request.url);
+  const postId = url.searchParams.get('postId');
+  if (postId === null) {
+    const postPreviews = await getPostPreview();
+    return NextResponse.json(postPreviews);
+  }
+  const post = await getPost({ postId });
+  return NextResponse.json(post);
 }
